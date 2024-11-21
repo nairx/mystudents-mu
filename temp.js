fea9042c527492aba478790e991c449070e38393
praveen <nairxprv@gmail.com>
2024-11-21 18:07:47 +0530

    first commit

---
+++ temp.js
@@ -0,0 +1,16 @@
+db.products.insertMany(
+    [
+    {
+        "name":"Product 5",
+        "desc":"This is the description of product 5",
+        "price":25,
+        "url":"https://picsum.photos/id/3/200/200"
+    },
+    {
+        "name":"Product 6",
+        "desc":"This is the description of product 6",
+        "price":32,
+        "url":"https://picsum.photos/id/3/200/200"
+    }
+]
+);
